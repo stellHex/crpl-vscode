92056f71f26640cdf5c39fc5fdab90bdf647c037
stellHex <stellatedhexahedron@gmail.com>
2018-11-06 19:02:22 -0600

    progress toward signatures

--- src/extension.ts
+++ src/extension.ts
@@ -26,6 +26,34 @@ const prefixPatterns = [
 ]
 const docPattern = /=====\s*(.*)\s*=====\s*(.*\s*)*?.*Arguments.*\^\s*\|\s*(.*?)\s*\|\s*(.*?)\s*\|(.*?)\|(.*\s*)*?===\s*Description.*\s*((.*\s*)*)/
 
+enum crplType {
+  b = 0b0001,
+  i = 0b0111,
+  f = 0b0101,
+  s = 0b0001,
+  l = 0b1001,
+  n = 0b0001,
+  o = 0b0000,
+}
+
+const sigSpec = {
+  convertible (t1: crplType, t2: crplType) { return (t1 & t2) === t2 },
+  inTypes: new Map([
+    ['b', crplType.b], ['i', crplType.i], ['f', crplType.f],
+    ['x', crplType.f], ['y', crplType.f], ['z', crplType.f],
+    ['s', crplType.s],
+    ['l', crplType.l],
+    ['n', crplType.n]
+  ]),
+  outTypes: new Map([
+    ['b', crplType.i], ['i', crplType.i], ['f', crplType.f],
+    ['x', crplType.i], ['y', crplType.i], ['z', crplType.f],
+    ['s', crplType.s],
+    ['l', crplType.l],
+    ['n', crplType.n]
+  ])
+}
+
 interface RichToken {
   token: string
   range: vscode.Range
@@ -124,7 +152,7 @@ class RichDoc {
     let tree: ParseTree = this.tokens.tree = new ParseTree()
 
     let spec = RichDoc.stackSpec
-    let stack: RichToken[] = []
+    let exeStack: RichToken[] = []
     let branch = tree
     let doc = this.doc
     let parametric = true
@@ -148,14 +176,14 @@ class RichDoc {
           if (token === '#') { mode = ParseMode.comment; id = 'comment'; wiki = false } 
           else {
             if (parametric) {
-              if (stack.length === 0) {
+              if (exeStack.length === 0) {
                 if (token[0] === '$') { id = 'define' }
                 else { parametric = false }
-              } else if (stack.length === 1 && stack[0].id === 'define') {
+              } else if (exeStack.length === 1 && exeStack[0].id === 'define') {
                 wiki = false
                 if (token === ':') { id = ':' }
                 else { error = 'Expected ":".'; parametric = false } 
-              } else if (stack.length === 1 && stack[0].id === ':') {
+              } else if (exeStack.length === 1 && exeStack[0].id === ':') {
                 wiki = false
                 if (RichDoc.valuePattern.test(token)) { id = 'value'; wiki = false }
                 else { error = 'Expected numeric literal or string.'; parametric = false }
@@ -164,7 +192,7 @@ class RichDoc {
                 parametric = false
               }
               if (error) {
-                stack.pop()
+                exeStack.pop()
               }
             }
             let lowerToken = token.toLowerCase()
@@ -195,7 +223,7 @@ class RichDoc {
 
           // go shallower
           if (id && spec.down.get(id)) {
-            let top = stack.pop()
+            let top = exeStack.pop()
             let topid = top ? top.id : parametric ? 'start' : 'bottom'
             let matcher = spec.down.get(id)
             let matches = matcher instanceof RegExp ? (<RegExp>matcher).test(<string>topid) : matcher === topid
@@ -204,13 +232,13 @@ class RichDoc {
             } else {
               let wrongness = spec.unstarted.get(id)
               rToken.error = rToken.error || `Unexpected token "${token}"${wrongness}.`
-              if (top) { stack.push(top) }
+              if (top) { exeStack.push(top) }
             }
           }
           addToken(rToken, line)
           // go deeper
           if (id && RichDoc.stackSpec.up.indexOf(id) > -1) {
-            stack.push(rToken)
+            exeStack.push(rToken)
             let newBranch = new ParseTree()
             branch.push(newBranch)
             branch = newBranch
@@ -229,10 +257,10 @@ class RichDoc {
       })
       if (mode === ParseMode.comment) { mode = ParseMode.normal }
     })
-    stack.forEach(rToken => {
+    exeStack.forEach(rToken => {
       if (rToken.id === 'func') {
         let wrongness = spec.unstarted.get(rToken.id)
-        rToken.error = (stack.length > 1) && `Unexpected token "${rToken.token}"${wrongness}.`
+        rToken.error = (exeStack.length > 1) && `Unexpected token "${rToken.token}"${wrongness}.`
         return
       }
       if (rToken.error) { return }
@@ -289,8 +317,11 @@ function docuWikiDocToMD (crplHTML: string): string[] {
     let matchedResult = docPattern.exec(docu)
     if (matchedResult === null) { throw 400 }
     let [, id,, args, results, notation,, description] = matchedResult
-    description = description.replace(/==+ *(.*?) *==+/g, `\n${splitKey}\n**$1**\n`);
-    description = description.replace(/\[\[.*\|(.*)\]\]/g, '$1')
+    description = description.replace(/==+ *(.*?) *==+/g, `\n${splitKey}\n**$1**\n`)
+      .replace(/\[\[.*\|(.*)\]\]/g, '$1')
+      .replace(/<\/?note>/g, '\n')
+      .replace(/<code>/g, '```crpl\n')
+      .replace(/<\/code>/g, '\n```')
     // replace docuWiki table with MD table
     let newDescription = description.replace(/\^(.*)\n/g, '|$1\n|\n');
     while (newDescription !== description) {
@@ -328,7 +359,57 @@ function completionFilter(completionList: string[], word: string, sortPrefix: st
   })
 }
 
+export async function scrapeSignatures(groupSize: number = 10) {
+  let groups: string[][] = []
+  let group: string[]
+
+  crplData.words.forEach((word, i) => {
+    if (!(i % groupSize)) { group = []; groups.push(group) }
+    group.push(word)
+  })
+  let result = []
+  for (let i = 0; i < groups.length; i++) {
+    console.log(i+1, '/', groups.length)
+    result.push(await Promise.all(groups[i].map(async word => {
+      type sigType = [(string|undefined)[], (string|undefined)[]]
+      let item: { [word:string]: 'ERROR' | sigType } = {}
+      try {
+        let docs = await request(crplData.prefix + word + crplData.suffix)
+
+        docs = docs.replace(/\r/g, '')
+        docs = (<RegExpExecArray>/=====(.*\n)*/.exec(docs))[0];
+        docs = docs.replace(/(===+\s*Examples|<\/textarea)(.*\n)*/, '');
+        docs = docs.replace(/''|%%/g, '`');
+        docs = entityDecode(docs)
+        let prepresig = /\| *`? *((?:[bifxysln]\d? *?)*) *-+ *((?:[bifxysln]\d? *?)*) *`? *\|$/i.exec(docs)
+        if (!prepresig) {
+          throw new Error(`Error getting signature from ${word}`)
+        }
+        let presig = (<(string|null|undefined)[]>prepresig).slice(1, 3)
+        let signature: sigType = [
+          presig[0] ? (<string>presig[0]).split(/ +/).map(t => t[0].toLowerCase()) : [],
+          presig[1] ? (<string>presig[1]).split(/ +/).map(t => t[0].toLowerCase()) : []
+        ]
+        item[word] = signature
+        return item
+      } catch (e) {
+        console.log(word, e)
+        item[word] = 'ERROR'
+        return item
+      }
+    })))
+  }
+
+  console.log(
+    JSON.stringify(result, null, ' ' )
+    .replace(/\n  ( +|(?=}))/g,'')
+    .replace( /\n [\[\]],?/g, '')
+    .replace(/},?\n  {/g, ',\n')
+  )
+}
+
 export function activate(context: vscode.ExtensionContext) {
+  // scrapeSignatures()
   let documents: Map<string, RichDoc> = new Map
   let diagnostics = vscode.languages.createDiagnosticCollection('crpl')
 
